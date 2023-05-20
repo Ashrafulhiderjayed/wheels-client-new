c7eef6bb4bfee6f795c361fff4fda566fe6e241f
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2023-05-20 08:19:56 +0600

    Gallery section added

---
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import img1 from '../../../assets/img/g1.jpg';
+import img2 from '../../../assets/img/g2.jpg';
+import img3 from '../../../assets/img/g3.jpg';
+import img4 from '../../../assets/img/g4.jpg';
+import img5 from '../../../assets/img/g5.jpg';
+import img6 from '../../../assets/img/g6.jpg';
+
+const Gallery = () => {
+  const images = [
+    {
+        src: img1,
+        alt: "",
+      },
+      {
+        src: img2,
+        alt: "",
+      },
+      {
+        src: img3,
+        alt: "",
+      },
+      {
+        src: img4,
+        alt: "",
+      },
+      {
+        src: img5,
+        alt: "",
+      },
+      {
+        src: img6,
+        alt: "",
+      },
+  ];
+
+  return (
+    <>
+    <h1 className="font-CreteRound text-center text-primary font-extrabold text-7xl my-8">Gallery</h1>
+    <div className="w-full p-5 pb-10 mb-10 gap-5 columns-3 space-y-5">
+      {images.map((image) => (
+        <img
+          key={image.src}
+          src={image.src}
+          alt={image.alt}
+        />
+      ))}
+    </div>
+    </>
+  );
+};
+
+export default Gallery;
