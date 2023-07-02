5c758d82b0760dfc1e40b3955ddcda6e8595df69
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2023-07-03 00:19:55 +0600

    rect-tilt npm package

--- src/Pages/CategoryDetails/CategoryDetails.jsx
+++ src/Pages/CategoryDetails/CategoryDetails.jsx
@@ -1,12 +1,26 @@
 import { Link } from "react-router-dom";
 import ReactStars from "react-stars";
+import { Tilt } from "react-tilt";
 
 const CategoryDetails = ({ toy }) => {
+  const defaultOptions = {
+    reverse:        false,  // reverse the tilt direction
+    max:            35,     // max tilt rotation (degrees)
+    perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
+    speed:          1000,   // Speed of the enter/exit transition
+    transition:     true,   // Set a transition on enter/exit.
+    axis:           null,   // What axis should be disabled. Can be X or Y.
+    reset:          true,    // If the tilt effect has to be reset on exit.
+    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+  }
     return (
         <div className="max-w-2xl mx-auto">
         <div className="bg-white shadow-md rounded-lg max-w-sm h-96 flex flex-col justify-between ">
           <div className="flex justify-center">
+          <Tilt options={defaultOptions} style={{ }}>
             <img className="rounded-t-lg p-8 h-60" src={toy.picture} alt="product image" />
+          </Tilt>
           </div>
           <div className="px-5 pb-5">
             <h3 className="text-gray-900 font-semibold text-xl tracking-tight ">
--- src/Pages/Home/Gallery/Gallery.jsx
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -1,3 +1,4 @@
+import { Tilt } from 'react-tilt';
 import img1 from '../../../assets/img/g1.jpg';
 import img2 from '../../../assets/img/g2.jpg';
 import img3 from '../../../assets/img/g3.jpg';
@@ -33,17 +34,31 @@ const Gallery = () => {
       },
   ];
 
+  const defaultOptions = {
+    reverse:        false,  // reverse the tilt direction
+    max:            35,     // max tilt rotation (degrees)
+    perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
+    speed:          1000,   // Speed of the enter/exit transition
+    transition:     true,   // Set a transition on enter/exit.
+    axis:           null,   // What axis should be disabled. Can be X or Y.
+    reset:          true,    // If the tilt effect has to be reset on exit.
+    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+  }
+
   return (
     <>
     <h1 className="font-CreteRound text-center text-primary font-extrabold text-6xl my-8">Gallery</h1>
     <div className="w-full p-5 pb-10 mb-10 gap-5 columns-3 space-y-5">
       {images.map((image) => (
+        <Tilt options={defaultOptions} style={{ }}>
         <img
           key={image.src}
           src={image.src}
           alt={image.alt}
-        />
-      ))}
+          />
+          </Tilt>
+        ))}
     </div>
     </>
   );
